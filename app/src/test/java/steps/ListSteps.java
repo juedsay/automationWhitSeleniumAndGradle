957c2735a2110ed138a2d86fcdc2ceac39a49144
Julian Simón <juedsay@gmail.com>
2023-12-05 11:26:57 -0300

    add a ListSteps with method's  steps of the Scenario to test

---
+++ app/src/test/java/steps/ListSteps.java
@@ -0,0 +1,33 @@
+package steps;
+
+import java.util.List;
+
+import io.cucumber.java.en.*;
+import pages.ListPage;
+
+public class ListSteps {
+    
+    ListPage list = new ListPage();
+
+    @Given("I navigate to the list page")
+    public void navigateToListPage(){
+        list.navigateToListPage();
+    }
+
+    @When("I search the list")
+    public void searchTheList() throws InterruptedException {
+        list.enterSearchCriteria();
+    }
+
+    @Then("I can find the text in the list")
+    public void theTableIsThere(){
+        List<String> lista = list.getAllSearchResults();
+        boolean textIsThere = lista.contains("Seatle, Washington");
+
+        if(textIsThere){
+            System.out.println("The text is on the list: PASSED.");
+        }else{
+            throw new Error("The text is not on the list: FAILED!");
+        }        
+    }
+}
