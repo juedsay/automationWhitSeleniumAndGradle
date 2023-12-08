d756107da99475ccb98c4e302d46ccaee8877714
Julian Simón <juedsay@gmail.com>
2023-12-08 15:18:27 -0300

    added class Hooks with a method to take an screenshot when a scenario is failed

---
+++ src/test/java/steps/Hooks.java
@@ -0,0 +1,24 @@
+package steps;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import pages.BasePage;
+
+public class Hooks extends BasePage{
+
+    public Hooks() {
+        super(driver);
+    }
+
+@After
+public void tearDown (Scenario scenario){
+    if(scenario.isFailed()){
+        scenario.write("This scenario failed!");
+        final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        scenario.embed(screenshot, "image/png");
+    }
+}
+    
+}
